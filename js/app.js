d0e28ed8504cac1239a57ebf94cc428234e2d715
unknown <agustinucavo@gmail.com>
2022-05-27 17:58:59 -0300

    mejora textos y agregado de contacto

--- js/app.js
+++ js/app.js
@@ -1,23 +1,70 @@
 'use strict'
 
-const grande = document.querySelector('.grande')
-const punto = document.querySelectorAll('.punto')
+function modificacionpagina(){
 
-punto.forEach((cadapunto, i )=> {
-    punto[i].addEventListener('click',()=>{
-    
-        let posicion = i
-        let operacion = i* -25 
-       // if (posicion==0 || posicion==1){
-          //  let operacion= 0
-        //}else{
-         //   let operacion= -100
-        //}
-        
-        grande.style.transform = `translateX(${ operacion }%)`
-        punto.forEach ((cadapunto , i )=>{
-            punto[i].classList.remove('activo')
-        })
-        punto[i].classList.add('activo')
-    })
-})
+    let dato= document.getElementById("selector_principal").value;
+
+    switch(dato){
+        case 'consulta':
+            document.getElementById("contratar").style.display="none";
+            document.getElementById("izq-contratar").style.display="none";
+            document.getElementById("der-contratar").style.display="none";
+            document.getElementById("asidepreguntas").style.display="flex";
+            document.getElementById("preguntas").style.display="flex";
+            document.getElementById("contacto").style.display="none";
+            document.getElementById("mapa").style.display="none";           
+            break;
+        case 'contratar':
+            document.getElementById("contratar").style.display="flex";
+            document.getElementById("izq-contratar").style.display="flex";
+            document.getElementById("der-contratar").style.display="flex";
+            document.getElementById("asidepreguntas").style.display="none";
+            document.getElementById("preguntas").style.display="none";
+            document.getElementById("contacto").style.display="none";
+            document.getElementById("mapa").style.display="none";
+            break;
+        case 'contacto':
+            document.getElementById("contratar").style.display="none";
+            document.getElementById("izq-contratar").style.display="none";
+            document.getElementById("der-contratar").style.display="none";
+            document.getElementById("asidepreguntas").style.display="none";
+            document.getElementById("preguntas").style.display="none";
+            document.getElementById("contacto").style.display="flex";
+            document.getElementById("mapa").style.display="initial";
+            break;
+        default:
+            window.alert("no deberia estar aqui");
+      }
+}
+function modificacionsuscripcion(){
+
+    let dato= document.querySelector('input[name="categoria"]:checked').value;
+
+    switch(dato){
+        case "P":
+                document.getElementById("izq-contratar").style.color="white";
+                document.getElementById("der-contratar").style.color="black";
+
+                document.getElementById("izq-contratar").style.borderColor="white";
+                document.getElementById("der-contratar").style.borderColor="black";
+                document.getElementById("der-contratar").style.boxShadow="5px 5px 5px lightgrey";
+                document.getElementById("izq-contratar").style.boxShadow="none";
+
+                break;
+        case "F":
+                document.getElementById("izq-contratar").style.color="black";
+                document.getElementById("der-contratar").style.color="white";
+
+                document.getElementById("der-contratar").style.borderColor="white";
+                document.getElementById("izq-contratar").style.borderColor="black";
+                document.getElementById("izq-contratar").style.boxShadow="-5px 5px 5px lightgrey";
+                document.getElementById("der-contratar").style.boxShadow="none";
+
+                break;
+            
+        default:
+    }
+}
+const e = new Event("change");
+const element = document.querySelector('#selector_principal');
+element.dispatchEvent(e);
